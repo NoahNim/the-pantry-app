d2fe9b1f3a1b0b0dd38eb98dbf0335c05a2b29d3
Noah Medoff <noahmedoff@gmail.com>
2024-08-04 21:15:54 -0700

    search function and app top bar

--- app/page.jsx
+++ app/page.jsx
@@ -20,6 +20,7 @@ import {
   deleteDoc,
   getDoc,
 } from "firebase/firestore";
+import AppBar from "./components/AppBar";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
@@ -87,9 +88,94 @@ export default function Home() {
   const handleClose = () => setOpen(false);
 
   return (
-    <main>
-      <div>
-      </div>
-    </main>
+    <>
+    <AppBar
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+    />
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction={"row"} spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                setItemName("");
+                handleClose();
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>
+        +
+      </Button>
+      <Box border={`1px solid ${theme.palette.divider}`}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#ADD8E6"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignContent={"center"}
+        >
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+            Inventory Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+          {filteredInventory.map(({ name, quantity }) => (
+            <Box
+              key={name}
+              width="100%"
+              height="150px"
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0"}
+              paddingX={5}
+            >
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                Quantity: {quantity}
+              </Typography>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                -
+              </Button>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
+  </>
   );
 }
